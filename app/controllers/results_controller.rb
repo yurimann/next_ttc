ce5898126197f59b43a0750b61d0e79bd114047d
Yuri Manahan <ymanahan@loyalty.com>
2017-11-02 14:27:23 -0400

    Add eastbound ttc and error logging

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -18,7 +18,7 @@ class ResultsController < ApplicationController
       end
     end
       res = result.sort_by {|obj| obj["minutes"]}
-      @results = res[0..4]
+      @results = res[0..2]
     #this is the string that will be passed to slack.
     x = 0
     until x >= @results.length do
@@ -36,10 +36,24 @@ class ResultsController < ApplicationController
   end
 
   def next
-    raw_data = [URI('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&r=504&s=1581'), URI('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&r=514&s=1581')]
+    if params[:text].downcase == "west"
+      raw_data = [URI('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&r=504&s=1581'), URI('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&r=514&s=1581')]
+    elsif params[:text].downcase == "east"
+      raw_data = [URI('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&r=504&s=3070'), URI('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&r=514&s=3070')]
+    else
+      response_data = "Please choose west or east"
+      respond_to do |format|
+        format.json{render json: response_data}
+      end
+      return response_data
+      render root_path
+    end
     result = []
     @result_string = ""
 
+    if params["content-type"] == nil
+      params["content-type"] = "json"
+    end
     #raw data comes from the TTC api in XML. Convert to ruby format with Nokogiri then into a hash so that it can be parsed.
     raw_data.each do |data|
       data_from_ttc = Net::HTTP.get(data)
@@ -51,20 +65,24 @@ class ResultsController < ApplicationController
       end
     end
       res = result.sort_by {|obj| obj["minutes"]}
-      @results = res[0..4]
+      @results = res[0..2]
     #this is the string that will be passed to slack.
     x = 0
     until x >= @results.length do
-      temp = "The #{@results[x]["branch"]} streetcar from King and Ontario to King Station will arrive in #{@results[x]["minutes"]} minute/s " + "\n"
-      @result_string += temp
-      x += 1
+      if params[:text].downcase == "west"
+        temp = "The #{@results[x]["branch"]} streetcar (#{@results[x]["vehicle"]}) from King and Ontario to King Station will arrive in #{@results[x]["minutes"]} minute/s " + "\n"
+      elsif params[:text].downcase == "east"
+        temp = "The #{@results[x]["branch"]} streetcar (#{@results[x]["vehicle"]}) from King Station to 351 King E will arrive in #{@results[x]["minutes"]} minute/s " + "\n"
+      end
+        @result_string += temp
+        x += 1
     end
-
 
     respond_to do |format|
       format.html
-      format.json {render json: @result_string.to_json}
+      format.json {render json: @result_string}
     end
+
   end
 
 end
