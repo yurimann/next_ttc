b0aac1f3a6c9fba92d9eaf3e426f8b61697d925f
Yuri Manahan <ymanahan@loyalty.com>
2017-11-02 11:50:22 -0400

    Fix json formatting of data for slack

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -3,9 +3,7 @@ $(document).on("ready", function(){
   var url = "https://hooks.slack.com/services/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";
   var text = $("#result").val();
   $.ajax({
-      data: 'payload=' + JSON.stringify({
-          "text": text
-      }),
+      data: 'payload=' + "{" + '"text"' + ":" + '"' + text + '"' + "}",
       dataType: 'json',
       processData: false,
       type: 'POST',
@@ -13,8 +11,8 @@ $(document).on("ready", function(){
   });
 
   // $.ajax({
-  //   // url: "https://peaceful-everglades-87508.herokuapp.com/next",
-  //   url: "/next",
+  //   url: "https://peaceful-everglades-87508.herokuapp.com/next.json",
+  //   // url: "/next",
   //   dataType: 'JSON',
   //   type: 'POST',
   //   data: {}
