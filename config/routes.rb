966fda6c2f3ef0cf6df0d1185bcbb544a24954b2
Yuri Manahan <ymanahan@loyalty.com>
2017-11-02 09:16:10 -0400

    Separated controller for get and post request

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "results#index"
-  post '/results', to: "results#index", as: "result"
+  post '/results', to: "results#next"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
